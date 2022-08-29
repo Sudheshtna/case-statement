ffdcda12bd64ec0332165fe6ac6b787fdf19498f
sudheshtnamupparapu1999@gmail.com <sudheshtnamupparapu1999@gmail.com>
2022-08-29 20:39:31 +0530

    Added: numdisplay

---
+++ numdisplay.sh
@@ -0,0 +1,13 @@
+read -p " enter number display:" number display
+case $numberdisplay in
+             1)echo"unit"
+                    ;;
+             10)echo"ten"
+                    ;;
+             100)echo"hundred"
+                    ;;
+             1000)echo"thousand"
+                    ;;
+             10000)echo"ten thousand"
+                    ;;
+esac
