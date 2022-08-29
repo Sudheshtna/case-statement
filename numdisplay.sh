f645b9c015c4d02efbc1a1c5b14f5b2a56943037
sudheshtnamupparapu1999@gmail.com <sudheshtnamupparapu1999@gmail.com>
2022-08-29 20:56:57 +0530

    Added: numdisplay

--- numdisplay.sh
+++ numdisplay.sh
@@ -1,13 +1,15 @@
-read -p " enter number display:" number display
+read -p " enter number :" number display
 case $numberdisplay in
-             1)echo"unit"
+             1)echo "unit"
                     ;;
-             10)echo"ten"
+             10)echo "ten"
                     ;;
-             100)echo"hundred"
+             100)echo "hundred"
                     ;;
-             1000)echo"thousand"
+             1000)echo "thousand"
                     ;;
-             10000)echo"ten thousand"
+             10000)echo "ten thousand"
+                    ;;
+             *)echo "invalid number"
                     ;;
 esac
